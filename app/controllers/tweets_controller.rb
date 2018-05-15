43215722d77f80fb024801053272f99edcfd58d8
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-15 13:57:52 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,4 +30,15 @@ class TweetsController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+
 end
