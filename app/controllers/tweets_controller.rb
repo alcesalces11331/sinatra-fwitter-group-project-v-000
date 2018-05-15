ff9f62731b21642430afb050e4646d06c1789eaf
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-15 14:50:45 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -40,5 +40,35 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      (@tweet.user == current_user) ? (erb :'/tweets/edit_tweet') : (redirect '/tweets')
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        (@tweet.user == current_user) ? (redirect to "/tweets/#{@tweet.id}") if @tweet.update(content: params[:content])) : (redirect '/tweets')
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.delete if @tweet.user == current_user
+    else
+      redirect 'login'
+    end
+  end
 
 end
